a8d8ba04eee33051229e164535ae88603dd7102f
Kirill Abroshin <kirillabrosink@gmail.com>
2026-03-26 01:39:17 +0700

    * new commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+	print("Hello, DevSecOps Lab!")
+
+
+if __name__ == "__main__":
+	main()
---
+++ run_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#  Запуск всех тестов в текущей директории
+for test_script in *.sh; do
+	if [[ -x "$test_script" ]]; then
+		echo "Запуск $test_script..."
+		./"$test_script"
+	else
+		echo "Тест ($test_script) пропущен, файл не исполняемый"
+	fi
+done
+
+echo "Все тесты завершены."
